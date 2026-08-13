61384bb00d22ceeac42dc31927703f094168f160
vishalgoudar07 <vishalgoudar143@gmail.com>
2026-08-13 15:08:34 +0530

    updated  with live

--- config/razorpay.php
+++ config/razorpay.php
@@ -1,7 +1,14 @@
 <?php
 
+// return [
+//     'key_id' => 'rzp_test_T0fj4y7pzCDK63',
+//     'key_secret' => 'qAqiIuU8Fm0VUmz0K0iI89Ba',
+//     'webhook_secret' => 'M8@KcQP@_X3aP_m'
+// ];
+
+
 return [
-    'key_id' => 'rzp_test_T0fj4y7pzCDK63',
-    'key_secret' => 'qAqiIuU8Fm0VUmz0K0iI89Ba',
-    'webhook_secret' => 'M8@KcQP@_X3aP_m'
+    'key_id' => 'rzp_live_TK4BdgkgcT6luh',
+    'key_secret' => 'WISWyBAwNRqYtbmOdLzYWlIO',
+    'webhook_secret' => '2027@Drivault'
 ];
